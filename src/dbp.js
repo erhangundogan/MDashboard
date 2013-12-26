bbcc9d25000c783707f19c6ad646da50206aac17
erhangundogan <erhan@trposta.net>
2013-12-26 23:18:50 +0200

    todo added, objects revised

--- src/dbp.js
+++ src/dbp.js
@@ -8,103 +8,24 @@
 var DBP, Dashboard, Widget;
 (function (global) {
 
+  /**
+   * DBP
+   * @returns {*}
+   * @constructor
+   */
   DBP = function () {
     this.options = {};
     return this;
   };
 
-  Dashboard = function (module) {
-    this.module = module;
-    this.options = {};
-    this.container = $('body');
-    this.widgets = [];
-    return this;
-  };
-  Dashboard.prototype.module = typeof DBP;
-  Dashboard.prototype.domReady = function (dashboard) {
-    dashboard.createDummy();
-  };
-  Dashboard.prototype.init = function (_options, callback) {
-    var self = this;
-    _.extend(self.options, _options);
-
-    var DOMLoadCheck = setInterval(function () {
-      var body = $('body');
-      if (body && body.length > 0) {
-        clearInterval(DOMLoadCheck);
-
-        if (callback) {
-          return callback(self);
-        } else {
-          self.domReady(self);
-        }
-      }
-    }, 200);
-  };
-  Dashboard.prototype.createDummy = function (module) {
-    var wrapper = $('<div class="gridster" />'),
-      list = $('<ul />'),
-      self = this;
-
-    self.container.append(wrapper.append(list));
-
-    var gridsterOptions = {
-      widget_margins: [10, 10],
-      widget_base_dimensions: [140, 140]
-    };
-
-    //var gridster = $('.gridster ul').gridster(gridsterOptions).data('gridster');
-
-    /*
-     for (var i = 0; i < 3; i++) {
-     var item = $('<li />'),
-     dataRow = Math.floor(Math.random() * 3) + 1,
-     dataColumn = Math.floor(Math.random() * 3) + 1,
-     dataSizeX = Math.floor(Math.random() * 3) + 1,
-     dataSizeY = Math.floor(Math.random() * 3) + 1;
-
-     item.attr('data-row', dataRow)
-     .attr('data-col', dataColumn)
-     .attr('data-sizex', dataSizeX)
-     .attr('data-sizey', dataSizeY);
-
-     list.append(item);
-     }
-     */
-    list.append($('<li />').attr('data-row', 1).attr('data-col', 1).attr('data-sizex', 1).attr('data-sizey', 3))
-      .append($('<li />').attr('data-row', 1).attr('data-col', 2).attr('data-sizex', 2).attr('data-sizey', 1))
-      .append($('<li />').attr('data-row', 2).attr('data-col', 2).attr('data-sizex', 1).attr('data-sizey', 1))
-      .append($('<li />').attr('data-row', 2).attr('data-col', 3).attr('data-sizex', 1).attr('data-sizey', 1))
-      .append($('<li />').attr('data-row', 3).attr('data-col', 2).attr('data-sizex', 1).attr('data-sizey', 1))
-      .append($('<li />').attr('data-row', 3).attr('data-col', 3).attr('data-sizex', 1).attr('data-sizey', 1));
-
-    list.gridster(gridsterOptions);
-
-    /*
-     var widgets = [
-     ['<li>0</li>', 1, 2],
-     ['<li>1</li>', 3, 2],
-     ['<li>2</li>', 3, 2],
-     ['<li>3</li>', 2, 1],
-     ['<li>4</li>', 4, 1],
-     ['<li>5</li>', 1, 2],
-     ['<li>6</li>', 2, 1]
-     ];
-
-     _.each(widgets, function (widget, i) {
-     gridster.add_widget.apply(gridster, widget);
-     });   */
-  };
-
-  Widget = function (dashboard) {
-    this.dashboard = dashboard;
-    return this;
-  }
-  Widget.prototype.dashboard = typeof Dashboard;
-
+  /**
+   * DBP Initialize
+   * @param _options DBP Options (optional)
+   * @returns {*} DBP module itself
+   */
   DBP.prototype.init = function (_options) {
     var self = this,
-      req = [_, $, $.fn.gridster];
+        req = [_, $, $.fn.gridster];
 
     for (var r in req) {
       if (typeof req[r] === 'undefined')
@@ -118,90 +39,91 @@ var DBP, Dashboard, Widget;
     return self;
   };
 
+  /**
+   * Dashboard construstor
+   * @param module DBP module owner
+   * @returns {*} Dashboar itself
+   * @constructor
+   */
+  Dashboard = function (module) {
+    this.module = module;
+    this.container = $('body');
+    this.widgets = [];
+    return this;
+  };
+  Dashboard.prototype.module = typeof DBP;
+
+  /**
+   *
+   * @param _options Dashboard options (optional)
+   * @param callback Callback when browser finished DOM operations (optional)
+   */
+  Dashboard.prototype.init = function (_options, callback) {
+    var self = this,
+        time = 0;
+
+    if (_.isFunction(_options)) {
+      callback = _options;
+      _options = {};
+    }
+
+    _.extend(self, _options);
+
+    if (!_options && !callback) {
+      return self;
+    } else {
+      var DOMLoadCheck = setInterval(function () {
+        var body = $('body');
+        if (body && body.length > 0) {
+          clearInterval(DOMLoadCheck);
+          callback(null, self);
+        } else if (time >= 10000) {
+          callback('DOM load timeout');
+        } else {
+          time += 200;
+        }
+      }, 200);
+    }
+  };
+
+  /**
+   * Crates dummy widgets
+   */
+  Dashboard.prototype.createDummy = function () {
+    var wrapper = $('<div class="gridster" />'),
+        list = $('<ul />'),
+        self = this,
+        gridsterOptions = {
+          widget_margins: [10, 10],
+          widget_base_dimensions: [310, 260]
+        };
+
+    self.container.append(
+      wrapper.append(
+        list));
+
+    //var widget = new Widget(options);
+
+    list.append($('<li />').attr('data-row', 1).attr('data-col', 1).attr('data-sizex', 1).attr('data-sizey', 3))
+        .append($('<li />').attr('data-row', 1).attr('data-col', 2).attr('data-sizex', 2).attr('data-sizey', 1))
+        .append($('<li />').attr('data-row', 2).attr('data-col', 2).attr('data-sizex', 1).attr('data-sizey', 1))
+        .append($('<li />').attr('data-row', 2).attr('data-col', 3).attr('data-sizex', 1).attr('data-sizey', 1))
+        .append($('<li />').attr('data-row', 3).attr('data-col', 2).attr('data-sizex', 1).attr('data-sizey', 1))
+        .append($('<li />').attr('data-row', 3).attr('data-col', 3).attr('data-sizex', 1).attr('data-sizey', 1))
+        .gridster(gridsterOptions);
+  };
+
+  /**
+   *
+   * @param _options Widget options (optional)
+   * @returns {*} Widget itself
+   * @constructor
+   */
+  Widget = function (_options) {
+    this.xSize = ySize = 1;
+    _.extend(this, _options);
+    return this;
+  };
+  Widget.prototype.dashboard = typeof Dashboard;
+
 }(this));
-
-/*
- Dashboard
- id
- name
- style
- class
- width
- minWidth
- maxWidth
- height
- minHeight
- maxHeight
- cellWidth
- cellHeight
- cellPadding
- rows
- columns
- widgets: array
- parent: Dashboard
- state: Changed
- container: jQuery object
- readonly: true/false
- save: function
- delete: function
- render: function
- responsive: true/false
- header
- footer
- events
- animation
- print
- export
-
- Widget
- id
- name
- style
- class
- width
- minWidth
- maxWidth
- height
- minHeight
- maxHeight
- parent
- header
- data
- footer
- state
- save
- delete
- render
- header
- footer
- events
- animation
- print
- export
-
- Chart
- id
- name
- style
- class
- width
- minWidth
- maxWidth
- height
- minHeight
- maxHeight
- data
- type
- series
- values
- options
- legend
- events
- animation
-
- renderLibrary
- getConfig
- getData
- async kullan
- dashboarder
- */
---
+++ todo.md
@@ -0,0 +1,93 @@
+## Dashboard
+
+* id
+* name
+* style
+* class
+* width
+* minWidth
+* maxWidth
+* height
+* minHeight
+* maxHeight
+* cellWidth
+* cellHeight
+* cellPadding
+* rows
+* columns
+* widgets: array
+* parent: Dashboard
+* state: Changed
+* container: jQuery object
+* readonly: true/false
+* save: function
+* delete: function
+* render: function
+* responsive: true/false
+* header
+* footer
+* events
+* animation
+* print
+* export
+
+---------------
+
+## Widget
+
+* id
+* name
+* style
+* class
+* width
+* minWidth
+* maxWidth
+* height
+* minHeight
+* maxHeight
+* parent
+* header
+* data
+* footer
+* state
+* save
+* delete
+* render
+* header
+* footer
+* events
+* animation
+* print
+* export
+
+---------------
+
+## Chart
+
+* id
+* name
+* style
+* class
+* width
+* minWidth
+* maxWidth
+* height
+* minHeight
+* maxHeight
+* data
+* type
+* series
+* values
+* options
+* legend
+* events
+* animation
+
+---------------
+
+## Features
+
+* renderLibrary
+* getConfig
+* getData
+* async
