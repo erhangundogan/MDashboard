6edb351a4ec01c797918f24c0cf7c85395f73e97
erhangundogan <erhan@trposta.net>
2013-12-30 17:10:27 +0200

    d3 static test graphics finished, padding bug fixed

--- src/mdashboard.js
+++ src/mdashboard.js
@@ -145,10 +145,10 @@ var MDashboard, MWidgetCollection, MWidget, MChart;
     _.extend(this, _options);
 
     this.width = (this.xSize * this.collection.columnWidth) +
-      ((this.xSize - 1) * this.collection.columnMargin);
+      (2 * ((this.xSize - 1) * this.collection.columnMargin));
 
-    this.height = (this.ySize * this.collection.rowHeight) +
-      ((this.ySize - 1) * this.collection.rowMargin);
+    this.height = (this.ySize * (this.collection.rowHeight)) +
+      (2 * ((this.ySize - 1) * this.collection.rowMargin));
 
     if (this.chart) {
       this.chart = new MChart(this, this.chart);
@@ -161,6 +161,13 @@ var MDashboard, MWidgetCollection, MWidget, MChart;
         if (self.header) {
           var headerHeight = self.header.height();
           self.height = self.height - headerHeight;
+
+          if (self.header.css) {
+            var headerPaddingTop = parseInt(self.header.css('padding-top')) || 0;
+            var headerPaddingBottom = parseInt(self.header.css('padding-bottom')) || 0;
+            self.height -= (headerPaddingTop + headerPaddingBottom);
+          }
+
           self.container.height(self.height);
         }
         self.isInitialized = true;
@@ -268,7 +275,7 @@ var MDashboard, MWidgetCollection, MWidget, MChart;
                 widget.container.append('<svg class="chart"></svg>');
 
                 var y = d3.scale.linear().range([height, 0]);
-                var chart = d3.select(".chart").attr("width", width).attr("height", height);
+                var chart = d3.select("#" + widget.id + " .chart").attr("width", width).attr("height", height);
                 y.domain([0, d3.max(this.dataset, function(d) { return d; })]);
 
                 var barWidth = width / this.dataset.length;
@@ -283,37 +290,173 @@ var MDashboard, MWidgetCollection, MWidget, MChart;
                   .attr("width", barWidth - 1);
 
                 bar.append("text")
-                  .attr("x", barWidth / 2)
+                  .attr("x", (barWidth / 2) - 4)
                   .attr("y", function(d) { return y(d) + 3; })
-                  .attr("dy", ".75em")
+                  .attr("dy", "1em")
+                  .attr("fill", "white")
                   .text(function(d) { return d; });
               }
             }
           },
           { row:1, col:2, xSize:2, header:'header 2' },
-          { row:2, col:2, header:'header 3' },
-          { row:2, col:3, header:'header 4', settings: false
-            /*chart: {
-              library:'d3.v3', type:'pie', dataset: {
-                'ATV': 20,
-                'KanalD': 35,
-                'NTV': 45
-              }
-            }*/
-          },
-          { row:3, col:2, settings: false },
-          { row:3, col:3, settings: false
-            /*chart: {
-              library:'d3.v3', type:'line', dataset: [
+          { row:2, col:2, ySize:2,
+            chart: {
+              library:'d3.v3',
+              type:'line',
+              dataset: [
                 { date:'10.05.2013', value:15  },
                 { date:'18.05.2013', value:102 },
                 { date:'20.06.2013', value:83  },
                 { date:'30.07.2013', value:155 },
                 { date:'08.09.2013', value:43  },
-                { date:'11.10.2013', value:62  }
-              ]
-            }*/
-          }
+                { date:'11.10.2013', value:62  },
+                { date:'18.10.2013', value:69  },
+                { date:'05.11.2013', value:43  },
+                { date:'11.12.2013', value:162 }
+              ],
+              style: function(widget) {
+                var chartStyle = [];
+                chartStyle.push("<style>");
+                chartStyle.push("#" + widget.id + " path { stroke: maroon; stroke-width: 1; fill: none; }");
+                chartStyle.push("#" + widget.id + " .axis { shape-rendering: crispEdges; }");
+                chartStyle.push("#" + widget.id + " .x.axis line, ");
+                chartStyle.push("#" + widget.id + " .x.axis path, ");
+                chartStyle.push("#" + widget.id + " .y.axis line, ");
+                chartStyle.push("#" + widget.id + " .y.axis path { fill: none; stroke: #000; }");
+                chartStyle.push("</style>");
+                return chartStyle = chartStyle.join(" ");
+              },
+              render: function(widget) {
+                var chart = this,
+                    margin = {top: 20, right: 20, bottom: 80, left: 50},
+                    width = widget.width - margin.left - margin.right,
+                    height = widget.height - margin.top - margin.bottom;
+
+                widget.container.append(chart.style(widget));
+                widget.container.append('<svg class="chart"></svg>');
+
+                var parseDate = d3.time.format("%d.%m.%Y").parse;
+
+                var x = d3.time.scale().range([0, width]);
+
+                var y = d3.scale.linear().range([height, 0]);
+
+                var xAxis = d3.svg.axis()
+                  .scale(x)
+                  .orient("bottom");
+
+                var yAxis = d3.svg.axis()
+                  .scale(y)
+                  .orient("left");
+
+                var line = d3.svg.line()
+                  .x(function(d) { return x(d.date); })
+                  .y(function(d) { return y(d.value); });
+
+                var svg = d3.select("#" + widget.id + " .chart")
+                  .attr("width", width + margin.left + margin.right)
+                  .attr("height", height + margin.top + margin.bottom)
+                  .append("g")
+                  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+                this.dataset.forEach(function(d) {
+                  d.date = parseDate(d.date);
+                  d.value = +d.value;
+                });
+
+                x.domain(d3.extent(this.dataset, function(d) { return d.date; }));
+                y.domain(d3.extent(this.dataset, function(d) { return d.value; }));
+
+                svg.append("g")
+                  .attr("class", "x axis")
+                  .attr("transform", "translate(0, " + height + ")")
+                  .call(xAxis)
+                  .selectAll("text")
+                    .style("text-anchor", "end")
+                    .attr("dx", "-.8em")
+                    .attr("dy", ".15em")
+                    .attr("transform", function(d) {
+                      return "rotate(-90)"
+                    });
+
+                svg.append("g")
+                  .attr("class", "y axis")
+                  .call(yAxis)
+                  .append("text")
+                  .attr("transform", "rotate(-90)")
+                  .attr("y", 6)
+                  .attr("dy", ".7em")
+                  .style("text-anchor", "end")
+                  .text("Değer");
+
+                svg.append("path")
+                  .datum(this.dataset)
+                  .attr("class", "line")
+                  .attr("d", line);
+              }
+            }
+          },
+          { row:2, col:3, header:'header 4', settings: false,
+            chart: {
+              library:'d3.v3',
+              type:'pie',
+              dataset: [
+                { label:'ATV', value:20 },
+                { label:'KanalD', value:35 },
+                { label:'NTV', value:45 }
+              ],
+              render: function(widget) {
+                var width = widget.width,
+                    height = widget.height,
+                    radius = Math.min.apply(Math, [widget.width/2, widget.height/2]) - 10,
+                    chart = this,
+                    color = d3.scale.category20c();
+
+                widget.container.append('<svg class="chart"></svg>');
+
+                var marginLeft = parseInt((width - (2 * radius)) / 2);
+                marginLeft = marginLeft > 0 ? marginLeft : 0;
+
+                var marginTop = parseInt((height - (2 * radius)) / 2);
+                marginTop = marginTop > 0 ? marginTop : 0;
+
+                var vis = d3.select("#" + widget.id + " .chart")
+                    .append("svg:svg")
+                    .data([this.dataset])
+                      .attr("width", width)
+                      .attr("height", height)
+                    .append("svg:g")
+                      .attr("transform", "translate(" + (radius + marginLeft) + "," + (radius + marginTop) + ")");
+
+                var arc = d3.svg.arc()
+                  .outerRadius(radius);
+
+                var pie = d3.layout.pie()
+                  .value(function(d) { return d.value; });
+
+                var arcs = vis.selectAll("g.slice")
+                  .data(pie)
+                  .enter()
+                  .append("svg:g")
+                  .attr("class", "slice");
+
+                    arcs.append("svg:path")
+                      .attr("fill", function(d, i) { return color(i); } )
+                      .attr("d", arc);
+
+                    arcs.append("svg:text")
+                      .attr("transform", function(d) {
+
+                        d.innerRadius = 0;
+                        d.outerRadius = radius;
+                        return "translate(" + arc.centroid(d) + ")";
+                      })
+                      .attr("text-anchor", "middle")
+                      .text(function(d, i) { return chart.dataset[i].label; });
+              }
+            }
+          },
+          { row:3, col:3, settings: false }
         ];
 
     _.each(widgets, function(options, index) {
