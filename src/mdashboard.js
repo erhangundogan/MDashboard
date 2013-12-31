5748b8ebfdac98f49e98c26d085c6d6b36f288b5
erhangundogan <erhan@trposta.net>
2013-12-31 14:54:06 +0200

    auto dimension feature added, highcharts added

--- src/mdashboard.js
+++ src/mdashboard.js
@@ -80,32 +80,74 @@ var MDashboard, MWidgetCollection, MWidget, MChart;
    * @constructor
    */
   MWidgetCollection = function (ownerDashboard, _options) {
+    var self = this;
     this.dashboard = ownerDashboard;
     this.order = ownerDashboard.collections.length + 1;
     this.isInitialized = false;
     this.collectionOptions = {
       widget_margins: [10, 10],
-      widget_base_dimensions: [310, 260],
+      //widget_base_dimensions: [360, 300],
       draggable: {
         handle: 'header'
       }
     };
-    this.container = $('body');
     this.widgets = [];
 
     if (_options) {
       _.extend(this, _options);
     }
 
-    this.columnWidth = this.collectionOptions.widget_base_dimensions[0];
-    this.rowHeight = this.collectionOptions.widget_base_dimensions[1];
+    if (!this.container) {
+      this.container = $('body');
+      $(window).bind('resize', function(event) {
+        self.events.onContainerResize(event, self);
+      });
+      this.height = $(window).height();
+    } else {
+      this.height = $(this.container).height();
+      $(this.container).bind('resize', function(event) {
+        self.events.onContainerResize(event, self);
+      });
+    }
+
+    this.width = $(this.container).width();
+
     this.columnMargin = this.collectionOptions.widget_margins[0];
     this.rowMargin = this.collectionOptions.widget_margins[1];
 
     return this;
   };
   MWidgetCollection.prototype.dashboard = typeof MDashboard;
+  MWidgetCollection.prototype.invalidate = function() {
+    var self = this;
 
+    self.collectionOptions.widget_base_dimensions = [self.width, self.height];
+
+    var xCount = yCount = 0;
+    _.each(self.widgets, function(widget, index) {
+      if (widget.row === 1) {
+        xCount += widget.xSize;
+      }
+      if (widget.col === 1) {
+        yCount += widget.ySize;
+      }
+    });
+
+    var xWidth = parseInt((self.width / xCount) - (2 * self.columnMargin));
+    var yHeight = parseInt((self.height / yCount) - (2 * self.rowMargin));
+    self.collectionOptions.widget_base_dimensions = [xWidth, yHeight];
+
+    self.columnWidth = xWidth;
+    self.rowHeight = yHeight;
+
+    _.each(self.widgets, function(widget, index) {
+      widget.width = (widget.xSize * self.columnWidth) +
+        (2 * ((widget.xSize - 1) * self.columnMargin));
+
+      widget.height = (widget.ySize * (self.rowHeight)) +
+        (2 * ((widget.ySize - 1) * self.rowMargin));
+    });
+  };
   MWidgetCollection.prototype.render = function() {
     var wrapper = $('<div class="gridster" />'),
         list = $('<ul />'),
@@ -122,8 +164,32 @@ var MDashboard, MWidgetCollection, MWidget, MChart;
     });
 
     list.gridster(self.collectionOptions);
-
     self.isInitialized = true;
+    return self;
+  };
+  MWidgetCollection.prototype.add = function(widget) {
+    var self = this;
+
+    if (_.isArray(widget)) {
+      _.each(widget, function(item, index) {
+        self.widgets.push(new MWidget(self, item));
+      });
+    } else if (_.isObject(widget)) {
+      self.widgets.push(new MWidget(self, widget));
+    }
+
+    self.events.onCollectionChange(self);
+
+    return self;
+  };
+  MWidgetCollection.prototype.events = {
+    onCollectionChange: function(collection) {
+      collection.invalidate();
+    },
+    onContainerResize: function(event, collection) {
+      //widget.resize();
+      collection.invalidate();
+    }
   };
 
   /**
@@ -144,12 +210,13 @@ var MDashboard, MWidgetCollection, MWidget, MChart;
 
     _.extend(this, _options);
 
+    /*
     this.width = (this.xSize * this.collection.columnWidth) +
       (2 * ((this.xSize - 1) * this.collection.columnMargin));
 
     this.height = (this.ySize * (this.collection.rowHeight)) +
       (2 * ((this.ySize - 1) * this.collection.rowMargin));
-
+*/
     if (this.contentType === "chart") {
       this.chart = new MChart(this, this.chart);
     }
@@ -216,6 +283,9 @@ var MDashboard, MWidgetCollection, MWidget, MChart;
           if (self.settings) {
             var settingsIcon = $('<i class="fa fa-cog fa-2x fa-white mdashboard-settings-icon"></i>');
             self.settingsIcon = settingsIcon;
+            settingsIcon.bind('click', function(event) {
+              self.events.onSettingsOpen(event, self);
+            });
             item.prepend(settingsIcon);
           }
           break;
@@ -223,6 +293,16 @@ var MDashboard, MWidgetCollection, MWidget, MChart;
     });
 
     return item;
+  };
+  MWidget.prototype.redraw = function() {
+    var self = this;
+
+    self.container.empty();
+  };
+  MWidget.prototype.events = {
+    onSettingsOpen: function(event, widget) {
+      console.log("onSettingsOpen");
+    }
   };
 
   MChart = function(ownerWidget, _options) {
@@ -246,328 +326,5 @@ var MDashboard, MWidgetCollection, MWidget, MChart;
     return this;
   };
   MChart.prototype.widget = typeof MWidget;
-
-  /*MChart.prototype.render = function(callback) {
-    var self = this;
-
-    if (!self.type) callback('Chart type not specified!');
-    if (!self.dataset) callback('Chart data not provided');
-
-    switch(self.library) {
-      case 'd3.v3':
-
-        break;
-    }
-  };*/
-
-  /**
-   * Crates dummy widgets
-   */
-  MWidgetCollection.prototype.createDummyWidgets = function () {
-    var self = this,
-        widgets = [
-
-          // mwidget-1
-          // bar chart
-          { row:1,
-            col:1,
-            ySize:3,
-            header:"header 1",
-            contentType:"chart",
-            chart: {
-              library:"d3.v3",
-              type:"bar",
-              dataset: [5, 12, 25, 8, 23, 7, 20],
-              render: function(widget) {
-                var width = widget.width,
-                    height = widget.height;
-
-                widget.container.append('<svg class="chart"></svg>');
-
-                var y = d3.scale.linear().range([height, 0]);
-                var chart = d3.select("#" + widget.id + " .chart").attr("width", width).attr("height", height);
-                y.domain([0, d3.max(this.dataset, function(d) { return d; })]);
-
-                var barWidth = width / this.dataset.length;
-                var bar = chart.selectAll("g")
-                  .data(this.dataset)
-                  .enter().append("g")
-                  .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
-
-                bar.append("rect")
-                  .attr("y", function(d) { return y(d); })
-                  .attr("height", function(d) { return height - y(d); })
-                  .attr("width", barWidth - 1);
-
-                bar.append("text")
-                  .attr("x", (barWidth / 2) - 4)
-                  .attr("y", function(d) { return y(d) + 3; })
-                  .attr("dy", "1em")
-                  .attr("fill", "white")
-                  .text(function(d) { return d; });
-              }
-            }
-          },
-
-          // mwidget-2
-          // empty
-          { row:1,
-            col:2,
-            xSize:2,
-            header:"header 2",
-            contentType:"html",
-            html: {
-              dataset: [
-                { label:"Feedback", value:32 },
-                { label:"Reservation", value:8 },
-                { label:"Meeting", value:4 },
-                { label:"Performance", value:11 }
-              ],
-              style: function(widget) {
-                var htmlStyle = [];
-                htmlStyle.push("<style>");
-                htmlStyle.push("#" + widget.id + " table.ui-crm-results { width:100%; height:100%; font-size:16pt; border-width:0 } ");
-                htmlStyle.push("#" + widget.id + " table.ui-crm-results tr:nth-child(odd) { background-color: #EEE; } ");
-                htmlStyle.push("#" + widget.id + " table.ui-crm-results tr td div.ui-crm-label { text-align: left; font-weight: bold } ");
-                htmlStyle.push("#" + widget.id + " table.ui-crm-results tr td div.ui-crm-result { text-align: center; } ");
-                htmlStyle.push("</style>");
-                return htmlStyle = htmlStyle.join(" ");
-              },
-              render: function(widget) {
-                var table = $("<table></table>").attr("padding", 0).attr("spacing", 0).addClass("ui-crm-results");
-
-                _.each(this.dataset, function(item, index) {
-                  var tr = $("<tr></tr>");
-
-                  tr.append(
-                    $("<td></td>").append(
-                      $("<div></div>").addClass("ui-crm-label").append(item.label)));
-                  tr.append(
-                    $("<td></td>").append(
-                      $("<div></div>").addClass("ui-crm-result").append(item.value)));
-
-                  table.append(tr);
-                });
-
-                widget.container.append(this.style(widget));
-                widget.container.append(table);
-              }
-            }
-          },
-
-          // mwidget-3
-          //line chart
-          { row:2,
-            col:2,
-            ySize:2,
-            contentType:"chart",
-            chart: {
-              library:"d3.v3",
-              type:"line",
-              dataset: [
-                { date:"10.05.2013", value:15  },
-                { date:"18.05.2013", value:102 },
-                { date:"20.06.2013", value:83  },
-                { date:"30.07.2013", value:155 },
-                { date:"08.09.2013", value:43  },
-                { date:"11.10.2013", value:62  },
-                { date:"18.10.2013", value:69  },
-                { date:"05.11.2013", value:43  },
-                { date:"11.12.2013", value:162 }
-              ],
-              style: function(widget) {
-                var chartStyle = [];
-                chartStyle.push("<style>");
-                chartStyle.push("#" + widget.id + " path { stroke: maroon; stroke-width: 1; fill: none; }");
-                chartStyle.push("#" + widget.id + " .axis { shape-rendering: crispEdges; }");
-                chartStyle.push("#" + widget.id + " .x.axis line, ");
-                chartStyle.push("#" + widget.id + " .x.axis path, ");
-                chartStyle.push("#" + widget.id + " .y.axis line, ");
-                chartStyle.push("#" + widget.id + " .y.axis path { fill: none; stroke: #000; }");
-                chartStyle.push("</style>");
-                return chartStyle = chartStyle.join(" ");
-              },
-              render: function(widget) {
-                var chart = this,
-                    margin = {top: 20, right: 20, bottom: 80, left: 50},
-                    width = widget.width - margin.left - margin.right,
-                    height = widget.height - margin.top - margin.bottom;
-
-                widget.container.append(chart.style(widget));
-                widget.container.append('<svg class="chart"></svg>');
-
-                var parseDate = d3.time.format("%d.%m.%Y").parse;
-
-                var x = d3.time.scale().range([0, width]);
-
-                var y = d3.scale.linear().range([height, 0]);
-
-                var xAxis = d3.svg.axis()
-                  .scale(x)
-                  .orient("bottom");
-
-                var yAxis = d3.svg.axis()
-                  .scale(y)
-                  .orient("left");
-
-                var line = d3.svg.line()
-                  .x(function(d) { return x(d.date); })
-                  .y(function(d) { return y(d.value); });
-
-                var svg = d3.select("#" + widget.id + " .chart")
-                  .attr("width", width + margin.left + margin.right)
-                  .attr("height", height + margin.top + margin.bottom)
-                  .append("g")
-                  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-                this.dataset.forEach(function(d) {
-                  d.date = parseDate(d.date);
-                  d.value = +d.value;
-                });
-
-                x.domain(d3.extent(this.dataset, function(d) { return d.date; }));
-                y.domain(d3.extent(this.dataset, function(d) { return d.value; }));
-
-                svg.append("g")
-                  .attr("class", "x axis")
-                  .attr("transform", "translate(0, " + height + ")")
-                  .call(xAxis)
-                  .selectAll("text")
-                    .style("text-anchor", "end")
-                    .attr("dx", "-.8em")
-                    .attr("dy", ".15em")
-                    .attr("transform", function(d) {
-                      return "rotate(-90)"
-                    });
-
-                svg.append("g")
-                  .attr("class", "y axis")
-                  .call(yAxis)
-                  .append("text")
-                  .attr("transform", "rotate(-90)")
-                  .attr("y", 6)
-                  .attr("dy", ".7em")
-                  .style("text-anchor", "end")
-                  .text("Değer");
-
-                svg.append("path")
-                  .datum(this.dataset)
-                  .attr("class", "line")
-                  .attr("d", line);
-              }
-            }
-          },
-
-          // mwidget-4
-          // pie chart
-          { row:2,
-            col:3,
-            header:'header 4',
-            settings: false,
-            contentType:"chart",
-            chart: {
-              library:'d3.v3',
-              type:'pie',
-              dataset: [
-                { label:'ATV', value:20 },
-                { label:'KanalD', value:35 },
-                { label:'NTV', value:45 }
-              ],
-              render: function(widget) {
-                var width = widget.width,
-                    height = widget.height,
-                    radius = Math.min.apply(Math, [widget.width/2, widget.height/2]) - 10,
-                    chart = this,
-                    color = d3.scale.category20c();
-
-                widget.container.append('<svg class="chart"></svg>');
-
-                var marginLeft = parseInt((width - (2 * radius)) / 2);
-                marginLeft = marginLeft > 0 ? marginLeft : 0;
-
-                var marginTop = parseInt((height - (2 * radius)) / 2);
-                marginTop = marginTop > 0 ? marginTop : 0;
-
-                var vis = d3.select("#" + widget.id + " .chart")
-                    .append("svg:svg")
-                    .data([this.dataset])
-                      .attr("width", width)
-                      .attr("height", height)
-                    .append("svg:g")
-                      .attr("transform", "translate(" + (radius + marginLeft) + "," + (radius + marginTop) + ")");
-
-                var arc = d3.svg.arc()
-                  .outerRadius(radius);
-
-                var pie = d3.layout.pie()
-                  .value(function(d) { return d.value; });
-
-                var arcs = vis.selectAll("g.slice")
-                  .data(pie)
-                  .enter()
-                  .append("svg:g")
-                  .attr("class", "slice");
-
-                    arcs.append("svg:path")
-                      .attr("fill", function(d, i) { return color(i); } )
-                      .attr("d", arc);
-
-                    arcs.append("svg:text")
-                      .attr("transform", function(d) {
-
-                        d.innerRadius = 0;
-                        d.outerRadius = radius;
-                        return "translate(" + arc.centroid(d) + ")";
-                      })
-                      .attr("text-anchor", "middle")
-                      .text(function(d, i) { return chart.dataset[i].label; });
-              }
-            }
-          },
-
-          // mwidget-5
-          // empty
-          { row:3,
-            col:3,
-            settings: false,
-            contentType:"chart",
-            chart: {
-              library:"google",
-              type:"gauge",
-              dataset: [
-                ["label", "value"],
-                ["Performance", 55]
-              ],
-              render: function(widget) {
-                google.load("visualization", "1", {packages:[this.type]});
-                google.setOnLoadCallback(drawChart);
-                function drawChart() {
-                  var data = google.visualization.arrayToDataTable(this.dataset);
-                  var options = {
-                    width: widget.width, height: widget.height,
-                    redFrom: 90, redTo: 100,
-                    yellowFrom:75, yellowTo: 90,
-                    minorTicks: 5
-                  };
-
-                  widget.container.append('<div class="chart"></div>');
-                  var container = $("#" + widget.id + " .chart");
-
-                  if (container && container.length > 0) {
-                    var chart = new google.visualization.Gauge(container[0]);
-                    chart.draw(this.dataset, options);
-                  }
-                }
-              }
-            }
-          }
-        ];
-
-    _.each(widgets, function(options, index) {
-      self.widgets.push(new MWidget(self, options));
-    });
-
-    self.render();
-  };
 
 }(this));
